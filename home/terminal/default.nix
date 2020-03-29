1bf0b1c0655ea3e4567f07fa8b1eadcc6707e162
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2020-03-29 16:04:58 +0200

    Set pywal template

--- home/terminal/default.nix
+++ home/terminal/default.nix
@@ -1,44 +1,40 @@
 { pkgs, ... }:
 
-let
-  env = import ../env.nix;
-  unstable = import <nixpkgs-unstable> {};
-in
 {
+  # Use my own st build
   nixpkgs.overlays = [
     (self: super: {
       st = super.st.overrideAttrs (oldAttrs: rec {
-        src = super.fetchFromGitHub {
-          repo = "st";
-          owner = "jwijenbergh";
-          rev = "eae6778842d02e9fcfc4fb4f9dbe6fc648bc56c1";
-          sha256 = "0cyya5bnw4rlx2zwsc1p2dlaiq1iia406lsd5nza5vq6rx77a0dm";
-        };
+        src = /home/jerry/Repos/st;
       });
     })
   ];
 
+  # Install it
   home.packages = [ pkgs.st ];
 
-  xresources.extraConfig = ''
-      st.font:       ${env.font}:pixelsize=14
-      st.foreground: ${env.theme.foreground}
-      st.background: ${env.theme.background}
-      st.color0:     ${env.theme.color0}
-      st.color8:     ${env.theme.color8}
-      st.color1:     ${env.theme.color1}
-      st.color9:     ${env.theme.color9}
-      st.color2:     ${env.theme.color2}
-      st.color10:    ${env.theme.color10}
-      st.color3:     ${env.theme.color3}
-      st.color11:    ${env.theme.color11}
-      st.color4:     ${env.theme.color4}
-      st.color12:    ${env.theme.color12}
-      st.color5:     ${env.theme.color5}
-      st.color13:    ${env.theme.color13}
-      st.color6:     ${env.theme.color6}
-      st.color14:    ${env.theme.color14}
-      st.color7:     ${env.theme.color7}
-      st.color15:    ${env.theme.color15}
-  '';
+  # Pywal template
+  xdg.configFile = 
+  {
+    "wal/templates/st".text = ''
+       st.foreground:       {foreground}
+       st.background:       {background}
+       st.color0:           {color0}
+       st.color8:           {color8}
+       st.color1:           {color1}
+       st.color9:           {color9}
+       st.color2:           {color2}
+       st.color10:          {color10}
+       st.color3:           {color3}
+       st.color11:          {color11}
+       st.color4:           {color4}
+       st.color12:          {color12}
+       st.color5:           {color5}
+       st.color13:          {color13}
+       st.color6:           {color6}
+       st.color14:          {color14}
+       st.color7:           {color7}
+       st.color15:          {color15}
+    '';
+  };
 }
