8f1ab2f8fadbbe77b05e3ea86319a48fdd664c5f
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2020-03-29 16:04:36 +0200

    Use nnn

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -4,35 +4,43 @@ let
   unstable = import <nixpkgs-unstable> {};
 in
   {
-  # Set shell packages
-  home.packages = with pkgs; [ unstable.any-nix-shell exa gotop lf ripgrep tealdeer ];
+    # Set shell packages
+    home.packages = with pkgs; [ 
+      unstable.any-nix-shell
+      bat
+      exa
+      gotop
+      nnn
+      ripgrep
+      tealdeer
+    ];
 
-  # Shell (+packages that need some configuration)
-  programs = {
-    fish = {
-      enable = true;
-      shellAbbrs = {
-        ls = "exa -l";
-        ll = "exa -al";
-        f = "lf";
+    # Shell + associated packages that need some configuration
+    programs = {
+      fish = {
+        enable = true;
+        shellAbbrs = {
+          ls = "exa -l";
+          ll = "exa -al";
+          f = "nnn";
+        };
+        promptInit = ''
+          any-nix-shell fish | source
+        '';
+        shellInit = ''
+          set -gx EDITOR nvim
+          function fish_greeting; end
+          set -U fish_prompt_pwd_dir_length 0
+        '';
       };
-      promptInit = ''
-        any-nix-shell fish | source
-      '';
-      shellInit = ''
-        set -gx EDITOR nvim
-        function fish_greeting; end
-        set -U fish_prompt_pwd_dir_length 0
-      '';
+      fzf = {
+        enable = true;
+        defaultCommand = "rg --files --hidden";
+      };
+      git = {
+        enable = true;
+        userName = "Jeroen Wijenbergh";
+        userEmail = "jeroenwijenbergh@protonmail.com";
+      };
     };
-    fzf = {
-      enable = true;
-      defaultCommand = "rg --files --hidden";
-    };
-    git = {
-      enable = true;
-      userName = "Jeroen Wijenbergh";
-      userEmail = "jeroenwijenbergh@protonmail.com";
-    };
-  };
-}
+  }
